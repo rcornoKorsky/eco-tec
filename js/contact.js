b07c49839082730b1c2c601befcc24f9298ac3da
Raul Cornejo Ortiz <r.cornejo003@ebc.edu.mx>
2025-05-22 09:32:41 -0600

    coding feature implementation of whatsapp function sender

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+"use strict";
+(function () {
+  document.getElementById("sendWhatsapp").addEventListener('click', () => {
+    const nameEl    = document.getElementById('name').value.trim();
+    const emailEl   = document.getElementById('email').value.trim();
+    const msgEl     = document.getElementById('message').value.trim();
+
+    if (!nameEl || !emailEl || !msgEl) {
+      alert('Por favor, completa todos los campos antes de enviar.');
+      return;
+    }
+
+    const text =
+      `*Hola, soy *%0A` + `${encodeURIComponent(nameEl)}%0A` +
+      `*Mi correo es: * ${encodeURIComponent(emailEl)}%0A` +
+      `** ${encodeURIComponent(msgEl)}`;
+
+
+    const phone = '527713631740';
+    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+    let url = isMobile ?  url = `whatsapp://send?phone=${phone}&text=${text}` :  url = `https://web.whatsapp.com/send?phone=${phone}&text=${text}`;
+    window.open(url, '_blank');
+  })
+}())
